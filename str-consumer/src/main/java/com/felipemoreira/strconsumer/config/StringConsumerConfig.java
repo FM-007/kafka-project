f000c18e7bc992f2a10e449cb4d9e20d6b478c4d
Felipe Moreira <felipe.g.silva@capgemini.com>
2022-12-28 22:12:14 -0300

    Starting microservice consumer

---
+++ str-consumer/src/main/java/com/felipemoreira/strconsumer/config/StringConsumerConfig.java
@@ -0,0 +1,42 @@
+package com.felipemoreira.strconsumer.config;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
+
+import java.util.HashMap;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@RequiredArgsConstructor
+@Configuration
+public class StringConsumerConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        var configs = new HashMap<String, Object>();
+        configs.put(BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        configs.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(configs);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> strConsumerFactory(
+        ConsumerFactory<String, String> consumerFactory
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory);
+
+        return factory;
+    }
+}
